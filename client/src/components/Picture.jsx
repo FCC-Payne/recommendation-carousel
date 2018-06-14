2ea215b1b19a4d0f4b698cae5a3724a6b93f37a6
fyun89 <fyun89@gmail.com>
2018-06-14 14:03:38 -0700

    fixed in accordance with PR suggestions

--- client/src/components/Picture.jsx
+++ client/src/components/Picture.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import CarouselListEntries from './CarouselListEntries.jsx';
 import styled from 'styled-components';
 //found interesting bug on Rent the Runway -- when image change fading image seems to be first one no matter what the previous image were. 
-import { ListEntry } from './CarouselListEntries.jsx'
-//changed from url(${image[3]}) to conserve GET request from S3 https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940
+
 const StyledImage4 = styled.div`
   position: relative;
   top: 5px;
@@ -22,7 +20,7 @@ const StyledImage4 = styled.div`
     opacity: 0;
   }
 `;
-// changed from url(${image[2]}) to conserve GET request from S3
+
 const StyledImage3 = styled.div`
   position: relative;
   top: -50px;
@@ -35,13 +33,13 @@ const StyledImage3 = styled.div`
   margin-left: 20px;
   margin-right: 20px;
   margin-top: -475px;
-  ${StyledImage4}:hover & {
+  &:hover {
     transition: opacity 0.5s ease-in-out;
     transition-delay: 1.5s;
     opacity: 0;
   }
 `;
-// changed from url(${image[1]}) to conserve GET request from S3
+
 const StyledImage2 = styled.div`
   position: relative;
   top: -210px;
@@ -53,13 +51,13 @@ const StyledImage2 = styled.div`
   background-size: 100%;
   margin-left: 20px;
   margin-right: 20px;
-  ${StyledImage4}:hover & {
+  &:hover {
     transition: opacity 0.5s ease-in-out;
     transition-delay: 3s;
     opacity: 0;
   }
 `;
-// changed from url(${image[0]}) to conserve GET request from S3
+
 const StyledImage1 = styled.div`
   position: relative;
   background: url(${props => props.image}); 
@@ -83,54 +81,6 @@ class Picture extends React.Component {
   }
   render(props) {
     let image = this.props.image;
-    // const productImage1 = {
-    //   position: 'relative',
-    //   background: `url(${image[0]})`, // changed from url(${image[0]}) to conserve GET request from S3
-    //   backgroundRepeat: 'no-repeat',
-    //   backgroundPosition: 'center',
-    //   width: '130px',
-    //   height: '210px',
-    //   backgroundSize: '100%',
-    //   marginLeft: '20px',
-    //   marginRight: '20px',
-    // };
-    // const productImage2 = {
-    //   position: 'relative',
-    //   top: '-210px',
-    //   background: `url(${image[1]})`, // changed from url(${image[1]}) to conserve GET request from S3
-    //   backgroundRepeat: 'no-repeat',
-    //   backgroundPosition: 'center',
-    //   width: '130px',
-    //   height: '210px',
-    //   backgroundSize: '100%',
-    //   marginLeft: '20px',
-    //   marginRight: '20px',
-    // };
-    // const productImage3 = {
-    //   position: 'relative',
-    //   top: '-525px',
-    //   background: `url(${image[2]})`, // changed from url(${image[2]}) to conserve GET request from S3
-    //   backgroundRepeat: 'no-repeat',
-    //   backgroundPosition: 'center',
-    //   width: '130px',
-    //   height: '420px',
-    //   backgroundSize: '100%',
-    //   marginLeft: '20px',
-    //   marginRight: '20px',
-    // };
-    // const productImage4 = {
-    //   position: 'relative',
-    //   top: '-945px',
-    //   background: `url(${image[3]})`, // changed from url(${image[3]}) to conserve GET request from S3
-    //   backgroundRepeat: 'no-repeat',
-    //   backgroundPosition: 'center',
-    //   width: '130px',
-    //   height: '420px',
-    //   backgroundSize: '100%',
-    //   marginLeft: '20px',
-    //   marginRight: '20px',
-    // };
-    console.log(image)
     return (
       <div>
         <StyledImage1 className="carouselImage1" image={image[0]} />
